58e1c92447215de3c6ab63a2ba91157147d79ca7
sib86 <epishev.artur2013@Yandex.ru>
2024-06-01 23:49:52 +0500

    16 commit

--- Work2/Task4/Program.cs
+++ Work2/Task4/Program.cs
@@ -5,12 +5,12 @@ int[] arr_int = new int[size];
 int i = 0;
 while(i < size)
 {
-    arr_int[i] = rnd.Next(1,10);
-    i++;
+    arr_int[i] = rnd.Next(1, 400);
+    i = i + 1;
 }
 i = 0;
-while(i < size);
+while(i < size)
 {
     Console.Write($"{arr_int[i]} ");
-i = i + 1;
+    i = i + 1;
 }
