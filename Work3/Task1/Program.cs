1754226c0721cb3a22421526c62265be3e226249
sib86 <epishev.artur2013@Yandex.ru>
2024-06-02 01:35:51 +0500

    17 commit

---
+++ Work3/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿int[,] Createtable(int rowCount, int columsCount)
+{
+    int[,] table = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return table;
+}
+
+void Showtable(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write($"{table[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] table = Createtable(4, 5);
+Showtable(table);
