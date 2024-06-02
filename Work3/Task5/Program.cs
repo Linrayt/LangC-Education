dc8b832a355141a79156d80cce748578137205f1
sib86 <epishev.artur2013@Yandex.ru>
2024-06-02 07:08:54 +0500

    22 commit

---
+++ Work3/Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿//ЗАДАЧА
+//Выдать на экран сообщение в случае наличия в массиве нечетного числа.
+//2,4,6 - компелятор проверил, на 7 он остановился с помощью break.
+
+Console.WriteLine("Start");
+
+int[] numbers = { 2, 4, 6, 7, 8, 10, 13, 16, 17, 24 };
+foreach (int number in numbers)
+{
+    if (number % 2 == 1)
+    {
+        Console.WriteLine("There is odd number");
+        break;
+    }
+}
+
+Console.WriteLine("Stop");
