6702f01b6e4df79155aa8536f432598e8e81cc0e
sib86 <epishev.artur2013@Yandex.ru>
2024-06-02 07:21:37 +0500

    23 commit

--- Work3/Task6/Program.cs
+++ Work3/Task6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//ЗАДАЧА:
+//Сообщить о наличии отрицательных элементах в строках двумерного массива.
+
+int[,] table = {{-1,1,1,2,6},
+            {2,2,27,6,4},
+            {3,3,-3,7,12} };
+for (int i = 0; i < table.GetLength(0); i++)
+    {
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        if (table[i, j] < 0)
+        {
+            Console.WriteLine($"There are negative elements in line {i}");
+            break;
+        }
+    }
+}
