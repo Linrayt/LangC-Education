6fefbf2b4678e5aa21d84adfd2e5eea7cfeb2e7b
sib86 <epishev.artur2013@Yandex.ru>
2024-06-02 07:37:52 +0500

    23 commit

---
+++ Work3/Task7/Program.cs
@@ -0,0 +1,13 @@
+﻿//ЗАДАЧА:
+//Если тебе нужно произвести 5 итераций, но на 3 проводить не надо, ее можно скипнуть с помощью оператора передачи управления - continue.
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write($"Iteration {i} ");
+    if (i == 3)
+    {
+        Console.WriteLine("Skip");
+        continue;
+    }
+    Console.WriteLine("Some processing");
+}
